145d8924b0e925b2e48c8a4e29aac6daac6e80ec
mohamed_bnz <simobnz81@gmail.com>
2026-02-04 15:50:46 +0100

    create new card

--- script.js
+++ script.js
@@ -12,10 +12,18 @@ let form = document.getElementById("form-container");
 let container = document.getElementById("posts-container");
 
 function afficherCards(tripsArray) {
-    container.innerHTML = ""; 
+    container.innerHTML = "";
     tripsArray.forEach(trip => {
         container.innerHTML += `
         <div class="mx-5 w-[350px] h-[150px] md:w-[250px] mt-16 mb-14">
+            <div class="absolute right-5 mb-10 flex gap-2 ">
+                <button onclick="editTrip(${trip.id}) ">
+                    <i class="fa-solid fa-pen text-blue-600"></i>
+                </button>
+                <button onclick="deleteTrip(${trip.id})">
+                    <i class="fa-solid fa-trash text-red-600"></i>
+                </button>
+            </div>
             <div class="bg-cover bg-center rounded-xl shadow-xl w-full h-full"
                  style="background-image:url('${trip.image}')"></div>
             <h6 class="font-semibold mt-1">${trip.title}</h6>
@@ -27,10 +35,27 @@ function afficherCards(tripsArray) {
 
 afficherCards(trips);
 
+form.addEventListener("click", function (e) {
+    if (e.target == form) {
+        form.classList.add("hidden")
+    }
+})
+
+
+function deleteTrip(id) {
+    trips=trips.filter(trip => trip.id !== id )
+    afficherCards(trips);
+
+}
+function editTrip(id) {
+    trips=trips.filter(t)
+    
+}
+
 function AddForm() {
     let btnAdd = document.getElementById("btnAdd");
     btnAdd.addEventListener("click", () => {
-        form.classList.toggle("hidden"); 
+        form.classList.toggle("hidden");
     });
 
     document.getElementById("trip-form").addEventListener("submit", function (e) {
@@ -44,10 +69,10 @@ function AddForm() {
             category: e.target.category.value
         };
 
-        trips.push(newTrip);   
-        afficherCards(trips);    
+        trips.push(newTrip);
+        afficherCards(trips);
         e.target.reset();
-        form.classList.add("hidden"); // nkhlli form ykhoufi mra ma t9edm
+        form.classList.add("hidden"); 
     });
 }
 
